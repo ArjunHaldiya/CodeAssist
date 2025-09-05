ea6c956686fca4ebc6a7627dbac87cf76902c0b1
Arjun Haldiya <arjunhaldiya91@gmail.com>
2025-09-05 10:44:40 -0700

    Test PR: add intentionally bad code

---
+++ pr_bad.py
@@ -0,0 +1,13 @@
+﻿def add(a,b): return a+b  # flake8: E231/E701
+eval("1+1")               # bandit: B307
+
+def complex_fn(x):
+    total = 0
+    for i in range(10):
+        if x > i:
+            if i % 2 == 0:
+                if x - i > 3:
+                    total += x*i
+                else:
+                    total -= i
+    return total
