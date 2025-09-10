42e16d94247f24b7ef5c92de3cad535420bef856
Arjun Haldiya <arjunhaldiya91@gmail.com>
2025-09-09 17:07:41 -0700

    Test: intentionally bad code to trigger CodeAssist

--- pr_bad.py
+++ pr_bad.py
@@ -1,2 +1,13 @@
 ﻿def add(a,b): return a+b  # flake8: spacing + one-line def
 eval("1+1")               # bandit: insecure
+
+def complex_fn(x):
+    total = 0
+    for i in range(10):
+        if x > i:
+            if i % 2 == 0:
+                if x - i > 3:
+                    total += x*i
+                else:
+                    total -= i
+    return total
